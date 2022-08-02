b8ff6535d45b5c8ab8dd2ad0f22fa7f18b57e8dc
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-02 22:00:10 +0530

    how to pass the html page

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
+const path = require('path')
 const express = require('express')
+// console.log(__dirname);
+// console.log(__filename);
+console.log(path.join(__dirname,'../public'));
 
 const app=express()
 
-app.get('',(req,res)=>{
-    res.send('hello express!')
-})
-app.get('/help',(req,res)=>{
-    res.send('<h1>help</h1>')
-})
-app.get('/about',(req,res)=>{
-    res.send({
-        name:'saurabh',
-        age:22
-    })
-})
-
-// app.get('/page',(req,res)=>{
-//     res.send(index.html)
+app.use(express.static(path.join(__dirname,'../public')))
+// app.get('',(req,res)=>{
+//     res.send('hello express!')
 // })
+// app.get('/help',(req,res)=>{
+//     res.send('<h1>help</h1>')
+// })
+// app.get('/about',(req,res)=>{
+//     res.send({
+//         name:'saurabh',
+//         age:22
+//     })
+// })
+
+
 app.listen(3000,()=>{
     console.log('server is start .....');
 })

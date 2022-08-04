2868008f355b63fa7aee2d76ff0e863dd1a1f6e3
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-04 14:09:52 +0530

    again i push the code for static page without any extra code

--- src/app.js
+++ src/app.js
@@ -1,28 +1,9 @@
-const path = require('path')
+const path=require('path')
 const express = require('express')
-// console.log(__dirname);
-// console.log(__filename);
-console.log(path.join(__dirname,'../public'));
+const app = express()
 
-const app=express()
-
-// app.use(express.static(path.join(__dirname,'../public')))
-
-// handelbars ------
-app.set('view engine','hbs')
-
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'saurabh',
-        age:'22'
-    })
-    // res.render('index')
-})
-
-
-
-
+app.use(express.static(path.join(__dirname,'../public')))
 
 app.listen(3000,()=>{
-    console.log('server is start .....');
+    console.log('server is start ...');
 })

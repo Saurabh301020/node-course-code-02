9b0d48c6326048f61c1be1f89076940aa60d3791
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-04 20:04:13 +0530

    paritals and change directory for views and imp use nodemon app.js -e js,hbs command

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
+const path = require('path')
 const express = require ('express')
+const hbs=require('hbs')
 const app = express()
 
+const viewsPath=path.join(__dirname,'../templates/view');
+const partialsPath=path.join(__dirname,'../templates/partials')
+
 app.set('view engine','hbs')
+app.set('views',viewsPath)
+
+hbs.registerPartials(partialsPath)
 
 app.get('',(req,res)=>{
     res.render('index',{

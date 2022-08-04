e1b7aa305757566a6d363c95c06d3da04faf1a45
saurabh_kumar <saurabhaggarwal2017@gmail.com>
2022-08-04 16:26:10 +0530

    dynamic rendering page using handlebars

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
-const path=require('path')
-const express = require('express')
+const express = require ('express')
 const app = express()
 
-app.use(express.static(path.join(__dirname,'../public')))
+app.set('view engine','hbs')
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Home',
+        name:'saurabh'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'help',
+        name:'saurabh'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'about',
+        name:'saurabh'
+    })
+})
 
 app.listen(3000,()=>{
-    console.log('server is start ...');
+    console.log('server is start ....');
 })
